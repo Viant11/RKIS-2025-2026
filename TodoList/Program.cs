6fcb09faef24fa71a1861d91c7a7714c6b497fb1
Evgeniy <soloviev4466@gmail.com>
2025-09-25 20:44:16 +0300

    TodoList

---
+++ TodoList/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TodoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работу выполнил Соловьёв Евгений и Тареев Юрий");
+            Console.WriteLine("Введите Имя");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Введите Фамилию");
+            string Surname = Console.ReadLine();
+            Console.WriteLine("Введите Год Рождения");
+            string date = Console.ReadLine();
+            int number = int.Parse(date);
+            string Date = DateTime.Now.ToString("yyyy");
+            int CurrentDate = int.Parse(Date); ;
+            int age = CurrentDate - number;
+            Console.WriteLine("Добавлен пользователь" + " " + Name + " " + Surname + " " + "возраст - " + age);
+        }
+    }
+}
