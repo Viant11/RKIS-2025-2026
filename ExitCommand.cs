c2fc4369513d3b35c1c324cd55bdc27182e99034
Evgeniy <soloviev4466@gmail.com>
2025-11-03 18:47:54 +0300

    Исправление ошибок

--- ExitCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿public class ExitCommand : ICommand
-{
-    public void Execute()
-    {
-        Console.WriteLine("Программа завершена.");
-        Environment.Exit(0);
-    }
-}
--- UnknownCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿public class UnknownCommand : ICommand
-{
-    public void Execute()
-    {
-        Console.WriteLine("Неизвестная команда. Введите команду help для просмотра доступных команд");
-    }
-}
