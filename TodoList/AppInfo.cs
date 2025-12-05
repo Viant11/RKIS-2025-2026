6e75cb66a3ffe206a428cf2de619a02ce022900b
Evgeniy <soloviev4466@gmail.com>
2025-12-05 22:23:38 +0300

    lesson11: update AppInfo

--- TodoList/AppInfo.cs
+++ TodoList/AppInfo.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public static class AppInfo
 {
 	public static TodoList Todos { get; set; }
-	public static Profile CurrentProfile { get; set; }
+	public static List<Profile> AllProfiles { get; set; }
+	public static Guid? CurrentProfileId { get; set; }
 
 	public static Stack<ICommand> UndoStack { get; set; }
 	public static Stack<ICommand> RedoStack { get; set; }
