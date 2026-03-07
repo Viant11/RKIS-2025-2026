a71d110d0713304314e7d5c8ba64eca4035090d7
Evgeniy <soloviev4466@gmail.com>
2026-03-07 20:19:18 +0300

    lesson2: make project preparation

---
+++ TodoList.Tests/TodoItemTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using System;
+
+namespace TodoList.Tests
+{
+	public class TodoItemTests
+	{
+		[Fact]
+		public void Constructor_ShouldSetDefaultValues()
+		{
+			string expectedText = "Купить хлеб";
+
+			var item = new TodoItem(expectedText);
+
+			Assert.Equal(expectedText, item.Text);
+			Assert.Equal(TodoStatus.NotStarted, item.Status);
+			Assert.True(item.LastUpdate <= DateTime.Now);
+		}
+
+		[Fact]
+		public void UpdateStatus_ShouldChangeStatusAndDate()
+		{
+			var item = new TodoItem("Test task");
+			var oldDate = item.LastUpdate;
+
+			System.Threading.Thread.Sleep(10);
+
+			item.UpdateStatus(TodoStatus.InProgress);
+
+			Assert.Equal(TodoStatus.InProgress, item.Status);
+			Assert.NotEqual(oldDate, item.LastUpdate);
+		}
+	}
+}
